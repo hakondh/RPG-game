934898abb5b439fc9a37100b0bdfde65374aa54d
Håkon Dalen Hestnes <hakonhd@live.no>
2021-01-22 13:12:41 +0100

    CHORE: Added constants to use throughout the project

---
+++ src/main/java/no/experis/constants/MageConstants.java
@@ -0,0 +1,15 @@
+package main.java.no.experis.constants;
+
+public class MageConstants {
+    // All the starting values for a mage
+    public static final int START_HP = 100;
+    public static final int START_STR = 2;
+    public static final int START_DEX = 3;
+    public static final int START_INT = 10;
+
+    // All the leveling values for a mage
+    public static final int LEVEL_UP_HP_GAIN = 15;
+    public static final int LEVEL_UP_STR_GAIN = 1;
+    public static final int LEVEL_UP_DEX_GAIN = 5;
+    public static final int LEVEL_UP_INT_GAIN = 5;
+}
---
+++ src/main/java/no/experis/constants/RangerConstants.java
@@ -0,0 +1,15 @@
+package main.java.no.experis.constants;
+
+public class RangerConstants {
+    // All the starting values for a ranger
+    public static final int START_HP = 120;
+    public static final int START_STR = 5;
+    public static final int START_DEX = 10;
+    public static final int START_INT = 2;
+
+    // All the leveling values for a ranger
+    public static final int LEVEL_UP_HP_GAIN = 20;
+    public static final int LEVEL_UP_STR_GAIN = 2;
+    public static final int LEVEL_UP_DEX_GAIN = 5;
+    public static final int LEVEL_UP_INT_GAIN = 1;
+}
